bb09e64c1baacf204e88e766fa8ab0c27ef49bae
Sean Sartell <ssartell@rightpoint.com>
2020-11-30 10:03:10 -0600

    init

---
+++ day01/test.js
@@ -0,0 +1,17 @@
+import assert from 'assert';
+import part1 from './part1.js';
+import part2 from './part2.js';
+
+describe('day01', function() {
+    describe('part 1', function() {
+        it('test', function() {
+            assert.equal(part1('asdf'), 'asdf');
+        });
+    });
+    
+    describe('part 2', function() {
+        // it('test', function() {
+        //     assert.equal(part1(input), 1);
+        // });
+    });
+});
