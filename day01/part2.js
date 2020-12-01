d26bafd4300436b1532475ce5a4d4ac323ef3f75
Sean Sartell <ssartell@rightpoint.com>
2020-12-01 09:57:28 -0600

    day01

--- day01/part2.js
+++ day01/part2.js
@@ -1,7 +1,13 @@
 import R from 'ramda';
+import combinations from '../utils/combinations.js';
 
-const debug = x => { debugger; return x; };
+const parseInput = R.pipe(R.trim, R.split('\n'), R.map(parseInt));
 
-const parseInput = R.pipe(R.trim);
+const findCombo = R.curry((sum, list) => {
+    for (const combo of combinations(list, 3)) {
+        if (R.sum(combo) === 2020) 
+            return R.product(combo);
+    }
+});
 
-export default R.pipe(parseInput, debug);
+export default R.pipe(parseInput, findCombo(2020));
