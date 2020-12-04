d433ed7040643f2e3844551c0bdcee8945557646
Sean Sartell <ssartell@rightpoint.com>
2020-12-04 08:14:22 -0600

    clean up

--- day04/part2.js
+++ day04/part2.js
@@ -3,10 +3,8 @@ import R from 'ramda';
 const readLine = R.pipe(R.trim, R.split(/\s+/), R.map(R.split(':')), R.fromPairs);
 const parseInput = R.pipe(R.trim, R.split('\r\n\r\n'), R.map(readLine));
 
-const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-const hasRequiredFields = R.pipe(R.keys, R.without(R.__, requiredFields), R.length, R.equals(0));
-
-const allValuesTrue = R.pipe(R.values, R.all(R.identity));
+const hasFields = fields => R.pipe(R.keys, R.without(R.__, fields), R.length, R.equals(0));
+const allFieldsValid = rules => R.pipe(R.evolve(rules), R.values, R.all(R.identity));
 
 const fourDigits = /^\d{4}$/;
 const nineDigits = /^\d{9}$/;
@@ -15,8 +13,8 @@ const height = /^(\d+)((?:cm)|(?:in))$/;
 const isMatch = R.curry((regex, str) => regex.test(str));
 const isBetween = R.curry((min, max, x) => min <= x && x <= max);
 const toHeight = R.pipe(R.match(height), R.tail, R.zipObj(['height', 'unit']));
-const isValidCm = R.pipe(toHeight, R.evolve({'unit': R.equals('cm'), 'height': isBetween(150, 193)}), allValuesTrue);
-const isValidIn = R.pipe(toHeight, R.evolve({'unit': R.equals('in'), 'height': isBetween(59, 76)}), allValuesTrue);
+const isValidCm = R.pipe(toHeight, allFieldsValid({'unit': R.equals('cm'), 'height': isBetween(150, 193)}));
+const isValidIn = R.pipe(toHeight, allFieldsValid({'unit': R.equals('in'), 'height': isBetween(59, 76)}));
 const isFoundIn = R.flip(R.contains);
 
 const validationRules = {
@@ -24,11 +22,11 @@ const validationRules = {
     'iyr': R.allPass([isMatch(fourDigits), isBetween(2010, 2020)]),
     'eyr': R.allPass([isMatch(fourDigits), isBetween(2020, 2030)]),
     'hgt': R.allPass([isMatch(height), R.anyPass([isValidCm, isValidIn])]),
-    'hcl': isMatch(hexColor),
-    'ecl': isFoundIn(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']),
-    'pid': isMatch(nineDigits),
-    'cid': R.T
+    'hcl': R.allPass([isMatch(hexColor)]),
+    'ecl': R.allPass([isFoundIn(['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'])]),
+    'pid': R.allPass([isMatch(nineDigits)]),
 };
-const allFieldsValid = R.pipe(R.evolve(validationRules), allValuesTrue);
 
-export default R.pipe(parseInput, R.filter(R.allPass([hasRequiredFields, allFieldsValid])), R.length);
+const requiredFields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
+
+export default R.pipe(parseInput, R.filter(R.allPass([hasFields(requiredFields), allFieldsValid(validationRules)])), R.length);
