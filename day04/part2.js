7263d51f30bc433dc3aaf4f943e7ee14990e4ce9
Sean Sartell <ssartell@rightpoint.com>
2020-12-04 08:18:41 -0600

    update

--- day04/part2.js
+++ day04/part2.js
@@ -3,6 +3,11 @@ import R from 'ramda';
 const readLine = R.pipe(R.trim, R.split(/\s+/), R.map(R.split(':')), R.fromPairs);
 const parseInput = R.pipe(R.trim, R.split('\r\n\r\n'), R.map(readLine));
 
+const fourDigits = /^\d{4}$/;
+const nineDigits = /^\d{9}$/;
+const hexColor = /^#([a-f]|[0-9]){6}$/;
+const height = /^(\d+)((?:cm)|(?:in))$/;
+
 const hasFields = fields => R.pipe(R.keys, R.without(R.__, fields), R.length, R.equals(0));
 const allFieldsValid = rules => R.pipe(R.evolve(rules), R.values, R.all(R.identity));
 const isMatch = R.curry((regex, str) => regex.test(str));
@@ -11,11 +16,6 @@ const toHeight = R.pipe(R.match(height), R.tail, R.zipObj(['height', 'unit']));
 const isValidCm = R.pipe(toHeight, allFieldsValid({'unit': R.equals('cm'), 'height': isBetween(150, 193)}));
 const isValidIn = R.pipe(toHeight, allFieldsValid({'unit': R.equals('in'), 'height': isBetween(59, 76)}));
 const isFoundIn = R.flip(R.contains);
-
-const fourDigits = /^\d{4}$/;
-const nineDigits = /^\d{9}$/;
-const hexColor = /^#([a-f]|[0-9]){6}$/;
-const height = /^(\d+)((?:cm)|(?:in))$/;
 
 const validationRules = {
     'byr': R.allPass([isMatch(fourDigits), isBetween(1920, 2002)]),
