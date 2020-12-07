09a487597dc7c0b4dc1f6b862a4cbbe2a08ecc63
Sean Sartell <ssartell@rightpoint.com>
2020-12-07 08:05:35 -0600

    day06

---
+++ day06/part1.js
@@ -0,0 +1,8 @@
+import R from 'ramda';
+
+const parseInput = R.pipe(R.split('\n\n'), R.map(R.split('\n')));
+
+export default R.pipe(parseInput, R.map(R.pipe(x => x.reduce(R.union), R.length)), R.sum);
+
+// import R from 'ramda';
+// export default R.pipe(R.split('\n\n'), R.map(R.split('\n')), R.map(R.pipe(x => x.reduce(R.union), R.length)), R.sum);
