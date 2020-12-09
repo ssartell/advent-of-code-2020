aafd1f6f2c5976e978591714bd2e0148d6578743
Sean Sartell <ssartell@rightpoint.com>
2020-12-09 15:32:04 -0600

    update

--- day09/part2.js
+++ day09/part2.js
@@ -5,23 +5,17 @@ const debug = x => { debugger; return x; };
 
 const parseInput = R.pipe(R.split('\n'), R.map(parseInt));
 
+const min = R.reduce(R.min, Infinity);
+const max = R.reduce(R.max, -Infinity);
+
 const findWeakness = data => {
     let num = firstInvalidXmas(25, data);
     
     for(let i = 0; i < data.length - 1; i++) {
-        if (data[i] > num) break;
         for(let j = i + 1; j < data.length; j++) {
-            if (data[j] > num) break;
-            let sum = 0;
-            let min = Infinity;
-            let max = -Infinity;
-            for(let k = i; k <= j; k++) {
-                sum += data[k];
-                min = Math.min(min, data[k]);
-                max = Math.max(max, data[k]);
-                if (sum > num) break;
-            }
-            if (sum === num) return min + max;
+            let slice = R.slice(i, j + 1, data);
+            let sum = R.sum(slice);
+            if (sum === num) return min(slice) + max(slice);
         }
     }
 }
